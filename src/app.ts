6ff9b57ec414303ebd93572ede2534d530cf8543
Rayssa_Bispo <bisporayssa6@gmail.com>
2025-11-06 16:25:14 -0300

    Feat: Imprementando a model Tarefa. #1

--- src/app.ts
+++ src/app.ts
@@ -4,59 +4,8 @@ import { Request, Response, NextFunction } from "express";
 const app = express();
 app.use(express.json())
 
-//rota quuery(Opcional): api/quartos/disponiveis ? inicio=2025
-
-app.get("/parametro/:nome",  (req:Request, res:Response, next:NextFunction) => {
- const nome = req.params.nome;
- console.log("Rota de parametro - Cliente digitou: ", nome)
- res.send(`Cliente digitou o nome: ${nome}`)
-})
-
-app.get("/query",  (req:Request, res:Response, next:NextFunction) => {
- const nome = req.query.nome
- console.log("Cliente digitou: ", nome)
- res.send(`voce digitou o nome: ${nome}`)
-})
-
-app.get("/corpo",  (req:Request, res:Response, next:NextFunction) => {
- const nome = req.body.nome
- console.log("Variavel dentro do corpo - Cliente digitou: ", nome)
- res.send(`voce digitou o nome: ${nome}`)
-})
-
-app.get("/exemplo",  (req:Request, res:Response, next:NextFunction) => {
-  console.log('aconteceu algo!!!')
-  res.send("rodou tudo certo!!")
-})
-
-//mais relevante para menos relevante
-
-// api/quartos/disponiveis
-// api/quartos/1
-// api/quartos/
 
 
-
-
-
-// Aqui terá todas as conexões
-
-
-
-
-//rota de erro
-app.use((req: Request, res: Response, next: NextFunction) => {
-  // rendler, quem está segurando as coisas
-
-  res.send("Hello Wold!!");
-});
-//rota generica
-
-app.use((err: Error, req: Request, res:Response, next: NextFunction) => {
-  // rendler, quem está segurando as coisas
-  console.log(err);
-  res.status(500).send("Erro na requisição")
-});
 
 
 // Rota Genérica, Vai pegar qualquer coisa que vir, irá processar o user
---
+++ src/models/task.ts
@@ -0,0 +1,19 @@
+//Representação de uma tarefa
+class Tarefa{
+
+  id: number;
+  nome: string;
+  descricao: string;
+
+  private static proximoID = 1;
+
+  //inicializar um construtor
+  constructor(nome:string, descricao:string){
+    this.id = Tarefa.proximoID++;
+    this.nome = nome;
+    this.descricao = descricao;
+    
+  }
+
+
+}
