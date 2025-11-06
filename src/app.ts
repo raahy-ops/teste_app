c9f48927465aeac60ae2f8f683d4d8738aa0b8fb
Rayssa_Bispo <bisporayssa6@gmail.com>
2025-11-06 15:24:54 -0300

    Feat|Test: escrito exemplos de acesso ao conteudo. #3

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,41 @@ import express from "express";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
+app.use(express.json())
+
+//rota quuery(Opcional): api/quartos/disponiveis ? inicio=2025
+
+app.get("/parametro/:nome",  (req:Request, res:Response, next:NextFunction) => {
+ const nome = req.params.nome;
+ console.log("Rota de parametro - Cliente digitou: ", nome)
+ res.send(`Cliente digitou o nome: ${nome}`)
+})
+
+app.get("/query",  (req:Request, res:Response, next:NextFunction) => {
+ const nome = req.query.nome
+ console.log("Cliente digitou: ", nome)
+ res.send(`voce digitou o nome: ${nome}`)
+})
+
+app.get("/corpo",  (req:Request, res:Response, next:NextFunction) => {
+ const nome = req.body.nome
+ console.log("Variavel dentro do corpo - Cliente digitou: ", nome)
+ res.send(`voce digitou o nome: ${nome}`)
+})
+
+app.get("/exemplo",  (req:Request, res:Response, next:NextFunction) => {
+  console.log('aconteceu algo!!!')
+  res.send("rodou tudo certo!!")
+})
 
 //mais relevante para menos relevante
 
 // api/quartos/disponiveis
 // api/quartos/1
-// api/quartos  
+// api/quartos/
+
+
+
 
 
 // Aqui terá todas as conexões
