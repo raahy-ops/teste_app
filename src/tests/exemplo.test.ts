6dfdf923d6d067c3265040a11a14571515908f83
Rayssa Bispo <bisporayssa6@gmail.com>
2025-12-03 14:32:55 -0300

    Feat: adicionar teste automatizado

---
+++ src/tests/exemplo.test.ts
@@ -0,0 +1,9 @@
+test("exemplo", ()=>{
+    const nome = "teste de carvalho"
+    expect(nome).toBe("Carvalho da Silva")
+})
+
+test("exemplo2", ()=>{
+    const result = 6+6;
+    expect(result).toBe(12)
+})
