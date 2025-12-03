228e9e832609fdff3d5925fdd40c5fce9da312eb
Rayssa Bispo <bisporayssa6@gmail.com>
2025-12-03 16:23:42 -0300

    feat : exemplo de teste

--- src/tests/exemplo.test.ts
+++
@@ -1,9 +0,0 @@
-test("exemplo", ()=>{
-    const nome = "teste de carvalho"
-    expect(nome).toBe("Carvalho da Silva")
-})
-
-test("exemplo2", ()=>{
-    const result = 6+6;
-    expect(result).toBe(12)
-})
---
+++ src/tests/tarefa.test.ts
@@ -0,0 +1,13 @@
+
+
+test("GET: /tarefas = 200", async()=>{
+    const res = await fetch("http://localhost:3000/tarefas")
+    expect(res.status).toBe(200)
+
+    const body = await res.json()
+    expect(Array.isArray(body)).toBe(true)
+})
+
+
+
+
