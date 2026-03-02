f878bf741f38fde5c73025f358ac4e2c9c68d006
Rayssa_Bispo <bisporayssa6@gmail.com>
2026-03-02 17:22:18 -0300

    agora vai 1.0

--- src/routes/clienteRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import clienteController from "../controllers/ClienteController";
- 
-const rotaClient = Router();
- 
-rotaClient.post("/", clienteController.Criarcliente);
- 
- 
-export default rotaLogin;
